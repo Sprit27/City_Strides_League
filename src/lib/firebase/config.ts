3544b7190c64e0ba53f3fe6aaae58082ca7f7497
Anish2710 <anishpawar04@gmail.com>
2026-02-26 16:20:22 +0000

    i want you to do some modifications, first that put all the api keys in

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -1,10 +1,8 @@
-
-// This file contains your actual Firebase project configuration.
 export const firebaseConfig = {
-  apiKey: "AIzaSyDrszL1_akAEdTrCc453Wq58G8HhJ_gNnk",
-  authDomain: "studio-9984507916-37681.firebaseapp.com",
-  projectId: "studio-9984507916-37681",
-  storageBucket: "studio-9984507916-37681.firebasestorage.app",
-  messagingSenderId: "737269970834",
-  appId: "1:737269970834:web:26a6d9961d3ef2bf67b34b",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
